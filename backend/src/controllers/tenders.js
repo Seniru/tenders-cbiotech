5bf2df1641096cebe34ac3d5ccf82f9184f1aff9
Seniru <senirupasan@gmail.com>
2025-01-01 22:33:23 +0530

    feat: api: Get tenders by date
    - Create /api/tenders/:date route

--- backend/src/controllers/tenders.js
+++ backend/src/controllers/tenders.js
@@ -6,17 +6,26 @@ const getTendersSummary = async (req, res) => {
         const itemNames = await Tender.distinct("itemName")
         const latestTenders = await Promise.all(
             itemNames.map(async (itemName) => {
-
-				// todo: quoted price should be quotedPriceLKR later
+                // todo: quoted price should be quotedPriceLKR later
                 const latestTender = await Tender.findOne({ itemName })
-					.populate({ path: "bidders", select: [ "bidder", "manufacturer", "currency", "quotedPrice" ] })
+                    .populate({
+                        path: "bidders",
+                        select: [
+                            "bidder",
+                            "manufacturer",
+                            "currency",
+                            "quotedPrice",
+                        ],
+                    })
                     .sort({ closedOn: -1 })
                     .exec()
 
-				if (latestTender && latestTender.bidders.length > 0) {
-					latestTender.bidders.sort((a, b) => a.quotedPrice - b.quotedPrice)
+                if (latestTender && latestTender.bidders.length > 0) {
+                    latestTender.bidders.sort(
+                        (a, b) => a.quotedPrice - b.quotedPrice,
+                    )
                     latestTender.bidders = [latestTender.bidders[0]]
-				}
+                }
 
                 return latestTender
             }),
@@ -28,6 +37,30 @@ const getTendersSummary = async (req, res) => {
     }
 }
 
+const getTendersOnDate = async (req, res) => {
+    try {
+        let { date } = req.params
+        let startDate = new Date(date)
+        let endDate = new Date(date)
+        endDate.setDate(endDate.getDate() + 1)
+
+        let tenders = await Tender.find({
+            closedOn: {
+                $gte: startDate,
+                $lt: endDate,
+            },
+        })
+            .populate("bidders")
+            .exec()
+
+        console.log(tenders)
+        res.send(tenders)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
 module.exports = {
-	getTendersSummary
+    getTendersSummary,
+    getTendersOnDate,
 }
--- backend/src/routes/tenders.js
+++ backend/src/routes/tenders.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-const { getTendersSummary } = require("../controllers/tenders")
+const {
+    getTendersSummary,
+    getTendersOnDate,
+} = require("../controllers/tenders")
 
 router.get("/", getTendersSummary)
+router.get("/:date", getTendersOnDate)
 
 module.exports = router
