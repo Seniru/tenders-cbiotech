e2d6976356f96075d7e877769b1ccc6e53e8dbf2
Seniru <senirupasan@gmail.com>
2025-01-24 00:25:14 +0530

    chore: Update sheet migrater

--- backend/src/extras/createFromSheets.js
+++ backend/src/extras/createFromSheets.js
@@ -15,10 +15,10 @@ const MONGO_URI = process.env.MONGO_URI
 
 const readBidders = (bidderDataColumns, worksheet, r) => {
     let bidders = []
-    while (worksheet.getRow(r).values.length > 0) {
+    while (worksheet.getRow(r).values.filter((v) => v && v.toString().trim() != "").length > 0) {
         let row = worksheet.getRow(r)
         let bidder = row.values[bidderDataColumns.bidder]
-        if (bidder.toLowerCase() == "no offers") return { bidders: [], row: r }
+        if (bidder.toLowerCase().startsWith("no offer")) return { bidders: [], row: r }
 
         bidders.push({
             bidder: row.values[bidderDataColumns.bidder],
@@ -28,7 +28,7 @@ const readBidders = (bidderDataColumns, worksheet, r) => {
             packSize: row.values[bidderDataColumns.packSize],
             bidBond: row.values[bidderDataColumns.bidBond] == "Yes",
             pr: row.values[bidderDataColumns.pr] == "Yes",
-            pca: bidderDataColumns.pca == -1 ? null : row.values[bidderDataColumns.pca],
+            pca: bidderDataColumns.pca == -1 ? null : row.values[bidderDataColumns.pca] == "Yes",
         })
         r++
     }
@@ -36,11 +36,12 @@ const readBidders = (bidderDataColumns, worksheet, r) => {
 }
 
 const skipUnnecessaryRows = (worksheet, r) => {
-    while (worksheet.getRow(r).getCell(3).value) r++
+    while (worksheet.getRow(r).getCell(3).value?.toString().trim()) r++
     return r
 }
 
 const createFromSheets = async () => {
+    let productCount = 0
     try {
         await mongoose.connect(MONGO_URI)
         console.log("Connected to MongoDB")
@@ -53,7 +54,7 @@ const createFromSheets = async () => {
         // read one file for now
         while ((dirent = await dir.read()) !== null) {
             if (!dirent.isFile()) continue
-            console.log("Opening", dirent.name)
+            console.log(++productCount, "Opening", dirent.name)
 
             let workbook = new ExcelJS.Workbook()
             await workbook.xlsx.readFile(`migrate/${dirent.name}`)
@@ -77,11 +78,26 @@ const createFromSheets = async () => {
                     let hours = parseInt(closedOn[1][0][0]) + (closedOn[1][1] == "PM" ? 12 : 0)
                     let mins = parseInt(closedOn[1][0][1])
 
+                    let itemName = worksheet.getRow(r + 1).values[3].trim()
+                    let tNumberFixed = worksheet.getRow(r + 2).values[3].trim()
+                    if (worksheet.getRow(r + 4).values[3]) {
+                        let sritem = worksheet
+                            .getRow(r + 4)
+                            .values[3].toString()
+                            .trim()
+                        sritem = sritem.split(/\s*-\s*/)
+                        if (sritem[1]) tNumberFixed += "/" + sritem[1]
+                    }
+                    if (tNumberFixed.includes("Urgent Purchase")) tNumberFixed += `/${itemName}`
+
                     tenders.push({
                         closedOn: new Date(Date.UTC(y, m - 1, d, hours, mins)),
-                        itemName: worksheet.getRow(r + 1).values[3].trim(),
-                        tenderNumber: worksheet.getRow(r + 2).values[3].trim(),
-                        quantity: worksheet.getRow(r + 3).values[3].trim(),
+                        itemName: itemName,
+                        tenderNumber: tNumberFixed,
+                        quantity: worksheet
+                            .getRow(r + 3)
+                            .values[3].toString()
+                            .trim(),
                         conversions: {},
                         bidders: [],
                     })
@@ -91,23 +107,33 @@ const createFromSheets = async () => {
                     let i = 0
                     while (!worksheet.getRow(r + i).values.some((v) => v.match("Bidder"))) {
                         // conversion is in the last column (popping returns the last value)
-                        let conversions = worksheet.getRow(r + i).values.filter((c) => c) // remove all the empty cell values
+                        let conversions = worksheet
+                            .getRow(r + i)
+                            .values.filter((c) => c?.trim() != "") // remove all the empty cell values
                         i++
                         if (conversions.length == 0) continue
                         for (let conversion of conversions) {
                             conversion = conversion.split(/[:=]/)
                             tenders[tenderNumber].conversions[
                                 conversion[0].match(/([a-zA-Z\s])+/)[0].trim()
-                            ] = parseFloat(conversion[1].match(/([\d.]+)/)[0].trim())
+                            ] = parseFloat(
+                                conversion[1]
+                                    .match(/([\d.,]+)/)[0]
+                                    .replace(/,/g, "")
+                                    .trim(),
+                            )
                         }
                     }
                     r += i
 
                     // bidder details
                     let availableBidderData = worksheet.getRow(r).values.map((v) => v.trim())
+                    let manufacturerCol = availableBidderData.indexOf("Manufacturer")
+                    if (manufacturerCol == -1)
+                        manufacturerCol = availableBidderData.indexOf("Manufacture")
                     let bidderDataColumns = {
                         bidder: availableBidderData.indexOf("Bidder"),
-                        manufacturer: availableBidderData.indexOf("Manufacturer"),
+                        manufacturer: manufacturerCol,
                         currency: availableBidderData.indexOf("Currency"),
                         quotedPrice: availableBidderData.indexOf("Quoted Price"),
                         packSize: availableBidderData.indexOf("Pack Size"),
